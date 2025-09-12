8cfdc5fbaa30a3ae8a59143de25112926f514d58
Francillon2024 <fra24017@byui.edu>
2025-09-11 23:23:16 -0400

    Adding Next.js Application Tutorial

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+// Primary font
+export const inter = Inter({ subsets: ['latin'] });
+
+// Secondary font
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
